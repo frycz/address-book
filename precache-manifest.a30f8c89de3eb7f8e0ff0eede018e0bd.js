335239a65673f5679a42535322d727083f0c9dd5
Adam Sawicki <adam.sawicki@symphony-solutions.eu>
2019-12-01 15:24:44 +0100

    Updates

---
+++ precache-manifest.a30f8c89de3eb7f8e0ff0eede018e0bd.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9da5db758930340bc215e66da5b5e4d8",
+    "url": "/address-book/index.html"
+  },
+  {
+    "revision": "86a89dab0538fb680fe4",
+    "url": "/address-book/static/css/4.f3273f2d.chunk.css"
+  },
+  {
+    "revision": "f3bc63ff2a4073275f10",
+    "url": "/address-book/static/css/5.25fbca42.chunk.css"
+  },
+  {
+    "revision": "0b456a052570394ffa55",
+    "url": "/address-book/static/css/main.a67fdb72.chunk.css"
+  },
+  {
+    "revision": "004cd06f71ba26d150f9",
+    "url": "/address-book/static/js/2.7bf8aa97.chunk.js"
+  },
+  {
+    "revision": "9adf9f0519727767309e",
+    "url": "/address-book/static/js/3.1e011e51.chunk.js"
+  },
+  {
+    "revision": "86a89dab0538fb680fe4",
+    "url": "/address-book/static/js/4.fc1d62a8.chunk.js"
+  },
+  {
+    "revision": "f3bc63ff2a4073275f10",
+    "url": "/address-book/static/js/5.0bc3347a.chunk.js"
+  },
+  {
+    "revision": "0b456a052570394ffa55",
+    "url": "/address-book/static/js/main.26218b25.chunk.js"
+  },
+  {
+    "revision": "16cdaf9ea4e702589ecc",
+    "url": "/address-book/static/js/runtime-main.f8524d91.js"
+  }
+]);
